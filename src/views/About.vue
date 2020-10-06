ce48b8738e5588b0136f17bf40cab5ce21b5c5bc
aljohn-ma <32862818+aljohn-ma@users.noreply.github.com>
2020-10-06 15:08:53 +0800

    router view inside suspense

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,18 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+function asyncSetTimeout(timeout) {
+  return new Promise(resolve => setTimeout(resolve, timeout));
+}
+
+export default {
+  async setup() {
+    await asyncSetTimeout(10000);
+    console.log("success");
+  }
+};
+</script>
+
+<style></style>
